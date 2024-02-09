71a95613b481bb00c60101cc96256c5a76568489
Salman-sajib <sfsajib@gmail.com>
2024-02-09 22:38:13 +0600

    used the userContext to the login and profile component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import UserContext from '../context/UserContext';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const { setUser } = useContext(UserContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setUser({ username, password });
   };
 
   return (
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react';
+import UserContext from '../context/UserContext';
+
 function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <h1>Not Logged In</h1>;
   return (
     <div>
-      <h2>Profile : {}</h2>{' '}
+      <h2>Profile : {user.username}</h2>
     </div>
   );
 }
