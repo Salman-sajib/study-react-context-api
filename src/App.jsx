f1367b45b8316f30fcb5b9f03e7445ce55805ce5
Salman-sajib <sfsajib@gmail.com>
2024-02-09 22:23:20 +0600

    created a context named userContext and added it to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Login from './components/Login';
 import Profile from './components/Profile';
-
+import UserContextProvider from './context/UserContextProvider';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <UserContextProvider>
       <h1>Study React Context API</h1>
       <Login />
       <Profile />
-    </div>
+    </UserContextProvider>
   );
 }
 
